07023a8d0ca72f4d0a7a0c8c25ae7ce6842eef9e
montigno <olivier.montigon@univ-grenoble-alpes.fr>
2026-03-13 14:52:39 +0100

    update software (see README)

--- skrypy-pyqt6/NodeEditor/python/Diagram_Editor.py
+++ skrypy-pyqt6/NodeEditor/python/Diagram_Editor.py
@@ -9144,7 +9144,7 @@ class ssh_diagram_execution():
                 fssh.write(pre_exec + "\n")
                 fssh.write("cd {}\n".format(host_skrypy_path))
                 fssh.write("source bin/activate\n")
-                fssh.write("cd skrypy\n")
+                fssh.write("cd skrypy-pyqt6\n")
                 fssh.write("python3 Execution_ssh.py {} {} {} {} {} {}\n".format(host_path, diagram, n_cpu, self.mode, opx, self.cluster))
                 fssh.write("deactivate\n")
                 fssh.write("echo\n")
